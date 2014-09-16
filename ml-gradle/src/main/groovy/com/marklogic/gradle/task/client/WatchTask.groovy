e077a76dbb27599339e8f05593d4b031d161f8a7
Rob Rudin <rob.rudin@marklogic.com>
2014-09-16 13:18:05 -0400

    Setting args via a method

--- ml-gradle/src/main/groovy/com/marklogic/gradle/task/client/WatchTask.groovy
+++ ml-gradle/src/main/groovy/com/marklogic/gradle/task/client/WatchTask.groovy
@@ -10,20 +10,19 @@ class WatchTask extends JavaExec {
     @TaskAction
     @Override
     public void exec() {
-        main = "com.marklogic.client.configurer.ml7.ConfigurationFilesWatcher"
-        classpath = getProject().sourceSets.main.runtimeClasspath
+        setMain("com.marklogic.client.configurer.ml7.ConfigurationFilesWatcher")
+        setClasspath(getProject().sourceSets.main.runtimeClasspath)
 
         AppConfig config = getProject().property("mlAppConfig")
         def username = config.getUsername() ? config.getUsername() : getProject().property("mlUsername")
         def password = config.getPassword() ? config.getPassword() : getProject().property("mlPassword")
-
-        args = [
+        setArgs([
             config.getConfigPaths().join(","),
             config.getHost(),
             config.getRestPort(),
             username,
             password
-        ]
+        ])
 
         super.exec()
     }
