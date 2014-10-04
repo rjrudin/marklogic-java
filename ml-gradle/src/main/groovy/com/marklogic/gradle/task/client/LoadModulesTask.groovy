e72a90b319d7fc76556c5050d87d0e2387fbe4dc
Rob Rudin <rob.rudin@marklogic.com>
2014-10-04 10:32:48 -0400

    Allowing for module paths to be overridden

--- ml-gradle/src/main/groovy/com/marklogic/gradle/task/client/LoadModulesTask.groovy
+++ ml-gradle/src/main/groovy/com/marklogic/gradle/task/client/LoadModulesTask.groovy
@@ -7,12 +7,14 @@ import com.marklogic.client.configurer.ml7.RestApiConfigurer
 
 class LoadModulesTask extends ClientTask {
 
+    List<String> modulePaths
+    
     @TaskAction
     void loadModules() {
         DatabaseClient client = newClient()
         RestApiConfigurer configurer = new RestApiConfigurer(client)
-        List<String> directories = getAppConfig().configPaths
-        println "Configuration paths: " + directories
+        List<String> directories = modulePaths != null ? modulePaths : getAppConfig().configPaths
+        println "Module paths: " + directories
         try {
             for (int i = 0; i < directories.size(); i++) {
                 String dir = directories.get(i);
