3045783754ada55ee8b505b7012998277571631e
Rob Rudin <rob.rudin@marklogic.com>
2014-10-09 13:45:16 -0400

    #26 Allowing for a collection to be deleted

--- ml-gradle/src/main/groovy/com/marklogic/gradle/task/database/ClearContentDatabaseTask.groovy
+++ ml-gradle/src/main/groovy/com/marklogic/gradle/task/database/ClearContentDatabaseTask.groovy
@@ -14,7 +14,18 @@ class ClearContentDatabaseTask extends ManageTask {
             return
         }
 
-        String xquery = "for \$forest-id in xdmp:database-forests(xdmp:database()) return xdmp:forest-clear(\$forest-id)"
+        String xquery = null;
+        
+        if (project.hasProperty("collection")) {
+            xquery = "xdmp:collection-delete('" + project.property("collection") + "')"
+        } else if (hasProperty("deleteAll")) {
+            xquery = "for \$forest-id in xdmp:database-forests(xdmp:database()) return xdmp:forest-clear(\$forest-id)"
+        } else {
+            println "To delete the documents in one collection, specify a collection via -Pcollection=name."
+            println "To delete all documents in the database, include the deleteAll parameter e.g. -PdeleteAll=true"
+            return
+        }
+        
         println "Clearing modules: " + xquery + "\n"
         new XccHelper(getDefaultXccUrl()).executeXquery(xquery)
     }
