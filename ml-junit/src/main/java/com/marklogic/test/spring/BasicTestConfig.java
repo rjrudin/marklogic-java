04229c40e795e2c23e269a15a2dbd9dbe0fadce7
Rob Rudin <rob.rudin@marklogic.com>
2014-10-12 22:43:54 -0400

    Extracted BasicConfig
    Can be used for main apps instead of tests

--- ml-junit/src/main/java/com/marklogic/test/spring/BasicTestConfig.java
+++ ml-junit/src/main/java/com/marklogic/test/spring/BasicTestConfig.java
@@ -1,40 +1,17 @@
 package com.marklogic.test.spring;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
-import com.marklogic.client.helper.DatabaseClientConfig;
-import com.marklogic.client.helper.DatabaseClientProvider;
-import com.marklogic.client.spring.SimpleDatabaseClientProvider;
-import com.marklogic.xcc.template.XccTemplate;
+import com.marklogic.client.spring.BasicConfig;
 
 /**
- * Provides a basic configuration for Spring-based tests. Assumes that properties can be found in the gradle.properties
- * file, though that file does not need to exist - this can be subclassed and a different property source can be used.
- * And since this is using Spring's Value annotation, system properties can be used to set all of the property values as
- * well.
+ * Extends BasicTestConfig with ports for a test application.
  */
 @Configuration
 @PropertySource({ "file:gradle.properties" })
-public class BasicTestConfig {
-
-    @Value("${mlUsername:admin}")
-    private String mlUsername;
-
-    @Value("${mlPassword}")
-    private String mlPassword;
-
-    @Value("${mlHost:localhost}")
-    private String mlHost;
-
-    @Value("${mlRestPort:0}")
-    private Integer mlRestPort;
-
-    @Value("${mlXdbcPort:0}")
-    private Integer mlXdbcPort;
+public class BasicTestConfig extends BasicConfig {
 
     @Value("${mlTestRestPort:0}")
     private Integer mlTestRestPort;
@@ -42,51 +19,14 @@ public class BasicTestConfig {
     @Value("${mlTestXdbcPort:0}")
     private Integer mlTestXdbcPort;
 
-    /**
-     * Has to be static so that Spring instantiates it first.
-     */
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
-        PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
-        c.setIgnoreResourceNotFound(true);
-        return c;
+    @Override
+    protected Integer getRestPort() {
+        return (mlTestRestPort != null && mlTestRestPort > 0) ? mlTestRestPort : getMlRestPort();
     }
 
-    @Bean
-    public DatabaseClientConfig databaseClientConfig() {
-        int restPort = (mlTestRestPort != null && mlTestRestPort > 0) ? mlTestRestPort : mlRestPort;
-        return new DatabaseClientConfig(mlHost, restPort, mlUsername, mlPassword);
-    }
-
-    @Bean
-    public DatabaseClientProvider databaseClientProvider() {
-        return new SimpleDatabaseClientProvider();
-    }
-
-    @Bean
-    public XccTemplate xccTemplate() {
-        int xdbcPort = (mlTestXdbcPort != null && mlTestXdbcPort > 0) ? mlTestXdbcPort : mlXdbcPort;
-        return new XccTemplate(String.format("xcc://%s:%s@%s:%d", mlUsername, mlPassword, mlHost, xdbcPort));
-    }
-
-    public String getMlUsername() {
-        return mlUsername;
-    }
-
-    public String getMlPassword() {
-        return mlPassword;
-    }
-
-    public String getMlHost() {
-        return mlHost;
-    }
-
-    public Integer getMlRestPort() {
-        return mlRestPort;
-    }
-
-    public Integer getMlXdbcPort() {
-        return mlXdbcPort;
+    @Override
+    protected Integer getXdbcPort() {
+        return (mlTestXdbcPort != null && mlTestXdbcPort > 0) ? mlTestXdbcPort : getMlXdbcPort();
     }
 
     public Integer getMlTestRestPort() {
@@ -96,4 +36,5 @@ public class BasicTestConfig {
     public Integer getMlTestXdbcPort() {
         return mlTestXdbcPort;
     }
+
 }
