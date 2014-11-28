4f60c0aa61d38511844734169efc7cd4dcc2444f
Rob Rudin <rob.rudin@marklogic.com>
2014-11-27 19:44:41 -0500

    Renamed

--- ml-gradle/src/main/groovy/com/marklogic/gradle/task/cpf/InstallAlertingPipelineTask.groovy
+++
@@ -1,8 +0,0 @@
-package com.marklogic.gradle.task.cpf
-
-class InstallAlertingPipelineTask extends InstallSystemPipelineTask {
-
-    public InstallAlertingPipelineTask() {
-        setFilename("Installer/alert/alerting-pipeline.xml")
-    }
-}
--- ml-gradle/src/main/groovy/com/marklogic/gradle/task/cpf/InstallSchPipelineTask.groovy
+++
@@ -1,8 +0,0 @@
-package com.marklogic.gradle.task.cpf
-
-class InstallSchPipelineTask extends InstallSystemPipelineTask {
-
-    public InstallSchPipelineTask() {
-        setFilename("Installer/cpf/status-pipeline.xml")
-    }
-}
